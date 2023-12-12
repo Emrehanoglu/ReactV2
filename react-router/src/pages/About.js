c4384423cfbc1f5022112ccf9640d945e2badf5c
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2023-12-12 21:53:16 +0300

    React Router #3

--- react-router/src/pages/About.js
+++ react-router/src/pages/About.js
@@ -1,6 +1,11 @@
+import { NavLink } from "react-router-dom"
 export const About = () => {
     return (
         <div className="about">
+            <nav>
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/about">About</NavLink>
+            </nav>
             <h1>Hakkımızda</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae consequuntur reprehenderit eligendi iusto, officia aliquid natus ad tempora perferendis aperiam voluptate qui, laudantium mollitia voluptatum laborum neque ex quis obcaecati.</p>
         </div>
--- react-router/src/pages/Home.js
+++ react-router/src/pages/Home.js
@@ -1,6 +1,12 @@
+import { NavLink } from "react-router-dom"
+
 export const Home = () => {
     return (
         <div className="home">
+            <nav>
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/about">About</NavLink>
+            </nav>
             <h1>Hoş geldiniz</h1>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet nesciunt voluptas accusamus fugiat expedita eius nam aperiam impedit architecto voluptatem error a officiis nisi, autem distinctio necessitatibus ipsam esse nostrum!</p>
         </div>
