7dcec75cbb9ceea43da2b45f0fbf06f8657e3285
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2023-12-15 20:53:47 +0300

    Yemek Tarifleri Uygulaması #2

--- yemek-tarifleri/src/App.js
+++ yemek-tarifleri/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import {RouterProvider, createBrowserRouter} from 'react-router-dom'
+import Home from './pages/home/Home'
+import Search from './pages/search/Search'
+import Details from './pages/details/Details'
+import Create from './pages/create/Create'
+
+const routes = createBrowserRouter([
+  {path: '/', element: <Home />},
+  {path: '/create', element: <Create />},
+  {path: '/details/:id', element: <Details />},
+  {path: '/search', element: <Search />},
+])
 
 function App() {
   return (
-    <div className="App">
-      <h1>Nefis Yemek Tarifleri</h1>
-    </div>
+    <RouterProvider router={routes}/>
   );
 }
 
